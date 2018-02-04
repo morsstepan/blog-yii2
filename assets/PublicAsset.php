5657e764efb0f529feffcdaaaba84151032fd4ff
Stepan Morozov <stepan.mors@gmail.com>
2018-02-04 23:06:09 +0300

    Added a simple markup

---
+++ assets/PublicAsset.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class PublicAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'public/css/bootstrap.min.css',
+        'public/css/font-awesome.min.css',
+        'public/css/animate.min.css',
+        'public/css/owl.carousel.css',
+        'public/css/owl.theme.css',
+        'public/css/owl.transitions.css',
+        'public/style.css',
+        'public/css/responsive.css',
+    ];
+    public $js = [
+        'text/javascript" src="assets/js/jquery-1.11.3.min.js',
+        'text/javascript" src="assets/js/bootstrap.min.js',
+        'text/javascript" src="assets/js/owl.carousel.min.js',
+        'text/javascript" src="assets/js/jquery.stickit.min.js',
+        'text/javascript" src="assets/js/menu.js',
+        'text/javascript" src="assets/js/scripts.js',
+    ];
+
+    public $depends = [
+
+    ];
+}
