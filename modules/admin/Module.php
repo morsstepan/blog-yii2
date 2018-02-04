949796b06a3aedd2587a85763e4852fdcee1024b
Stepan Morozov <stepan.mors@gmail.com>
2018-02-04 15:25:18 +0300

    Created an admin panel

---
+++ modules/admin/Module.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\modules\admin;
+
+/**
+ * admin module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    // public $layout = '/admin';
+    public $controllerNamespace = 'app\modules\admin\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        // путь к директории layouts модуля
+        \Yii::$app->setLayoutPath('@app/modules/admin/views/layouts');
+        // файл "admin.php"
+        \Yii::$app->layout = 'admin';
+    }
+}
