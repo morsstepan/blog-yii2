a94ebab66702c280cd6a47759e3693dd524f0f23
Stepan Morozov <stepan.mors@gmail.com>
2018-02-06 21:16:18 +0300

    Created a file for an auth controller

---
+++ controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\LoginForm;
+use Yii;
+use yii\web\Controller;
+
+class AuthController extends Controller
+{
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+    
+}
